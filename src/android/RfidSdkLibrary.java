0c8b2e1665e238063b5a3d8d65736afd98bcbf84
Matteo Moschelli <matteo.moschelli@anseltechnology.it>
2025-04-10 16:29:02 +0200

    Implement rfid methods

--- src/android/RfidSdkLibrary.java
+++ src/android/RfidSdkLibrary.java
@@ -2,6 +2,10 @@ package it.anseltechnology.rfid.sdklibrary.plugin;
 
 import org.apache.cordova.CordovaPlugin;
 
+import java.util.Locale;
+
+import javax.security.auth.callback.Callback;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
@@ -17,6 +21,7 @@ import androidx.lifecycle.Observer;
 import it.anseltechnology.rfid.sdklibrary.RfidLibraryInterface;
 import it.anseltechnology.rfid.sdklibrary.core.enums.ConnectionState;
 import it.anseltechnology.rfid.sdklibrary.core.interfaces.callbacks.OnConnectionStateListener;
+import it.anseltechnology.rfid.sdklibrary.core.interfaces.callbacks.OnRfidResultListener;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -25,9 +30,13 @@ public class RfidSdkLibrary extends CordovaPlugin {
 
     private static final String INIT = "init";
     private static final String CONNECT = "connect";
+    private static final String START_RFID = "start_rfid";
+    private static final String STOP_RFID = "stop_rfid";
 
     CallbackContext myCallbackContext;
     private RfidLibraryInterface rfidInterface;
+
+    private static CallbackContext myRfidCallbackContext = null;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -44,6 +53,18 @@ public class RfidSdkLibrary extends CordovaPlugin {
             String connectType = args.getString(0);
 
             this.connectDevice(connectType);
+
+            return true;
+        }
+        else if (action.equals(START_RFID)) {
+            myRfidCallbackContext = callbackContext;
+
+            this.startRfidScan();
+
+            return true;
+        }
+        else if (action.equals(STOP_RFID)) {
+            this.stopRfidScan();
 
             return true;
         }
@@ -70,6 +91,9 @@ public class RfidSdkLibrary extends CordovaPlugin {
             this.rfidInterface.setOnConnectionStateListener(new OnConnectionStateListener() {
                 public void onConnectionStateChange(ConnectionState state) {
                     if (state == ConnectionState.CONNECTED) {
+                        
+                        this.setRfidListener();
+
                         myCallbackContext.success("Device connected!");
                     }
                 }
@@ -81,5 +105,51 @@ public class RfidSdkLibrary extends CordovaPlugin {
             e.printStackTrace();
             myCallbackContext.error(e.getMessage());
         }
+    }
+
+    private void startRfidScan() {
+
+        thsi.rfidInterface.startRfidScan(false);
+
+        // Send no result for synchronous callback
+        PluginResult pluginresult = new PluginResult(PluginResult.Status.NO_RESULT);
+        pluginresult.setKeepCallback(true);
+        this.myCallbackContext.sendPluginResult(pluginresult);
+    }
+
+    private void stopRfidScan() {
+        this.rfidInterface.stopRfidScan();
+
+        myCallbackContext.success();
+    }
+
+    private void setRfidListener() {
+        this.rfidInterface.setOnRfidResultListener(new OnRfidResultListener() {
+            public void onRfidFound(String rfid) {
+                myRfidCallbackContext.success("{\"status\":\"OK\", \"body\":\""+rfid+"\"}");
+                myRfidCallbackContext = null;
+            }
+
+            public void onRfidNotFound() {
+                
+            }
+
+            public void onRfidTooManyFound(int tagNumber) {
+                myRfidCallbackContext.success("{\"status\":\"KO\", \"error\":\"Troppi tag in campo (letti "+ tagNumber +" tag)\"}");
+                myRfidCallbackContext = null;
+            }
+
+            public void onRfidInventory() {
+
+            }
+
+            public void onRfidReadStart() {
+                
+            }
+
+            public void onRfidReadStop() {
+                
+            }
+        });
     }
 }
--- www/rfidsdklibrary.js
+++ www/rfidsdklibrary.js
@@ -7,3 +7,11 @@ exports.init = function (arg0, success, error) {
 exports.connect = function (arg0, success, error) {
     exec(success, error, 'RfidSdkLibrary', 'connect', [arg0]);
 };
+
+exports.startRfidScan = function(success, error) {
+    exec(success, error, 'RfidSdkLibrary', 'start_rfid');
+}
+
+exports.stopRfidScan = function(success, error) {
+    exec(success, error, 'RfidSdkLibrary', 'stop_rfid');
+}
